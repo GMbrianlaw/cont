473ff201c7db494e10f21e4f09063076126cf980
blaw105 <blaw105@student.fuhsd.org>
2024-08-30 19:41:01 +0000

    Plz work

--- .lock.sh
+++ .lock.sh
@@ -3,9 +3,9 @@ cd
 code --install-extension oliver-ni.web-cat-vscode
 code --install-extension vscjava.vscode-java-pack
 
-mkdir -p .vscode-remote/extensions
+# mkdir -p .vscode-remote/extensions
 
-cd .vscode-remote
+# cd .vscode-remote
 
-sudo chown root:root extensions
-sudo chmod 700 extensions
+# sudo chown root:root extensions
+# sudo chmod 700 extensions
