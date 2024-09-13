c0c3a2a6f44c884f79682823ba457a4673ea4193
blaw105 <blaw105@student.fuhsd.org>
2024-09-13 19:00:59 +0000

    Add the check

--- .create.sh
+++ .create.sh
@@ -1,2 +1,9 @@
+if ! [ -x "$(command -v code)" ];
+then
+    cd
+    mkdir not_installed
+    exit
+fi
+
 code --install-extension oliver-ni.web-cat-vscode
 code --install-extension vscjava.vscode-java-pack
