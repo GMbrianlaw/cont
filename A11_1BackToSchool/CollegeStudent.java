bf8eabfb019ea9fc892d0966bd7444af352b405d
Brian Law <gmbrianlaw@gmail.com>
2024-11-01 19:06:52 +0000

    Diff of 904 chars

---
+++ A11_1BackToSchool/CollegeStudent.java
@@ -0,0 +1,18 @@
+public class CollegeStudent extends Student
+{
+    // TODO instance variables
+    
+    // TODO constructor
+    
+    // TODO methods
+
+    /**
+     * Returns a String representation of this class.
+     * @return private instance data as a String
+     */
+    public String toString()
+    {
+        return super.toString() + ", year: " + myYear +
+            ", major: " + myMajor;
+    }
+}
