0d3154085cdf2eb681fe345e6228f62479571755
Brian Law <gmbrianlaw@gmail.com>
2024-11-06 20:06:20 +0000

    197.489 secs passed

--- A11_3Checker/Checker.java
+++ A11_3Checker/Checker.java
@@ -1,21 +1,10 @@
-/**
- *  A checker is an object that examines strings and accepts those strings
- *  that meet a particular criterion.
- *
- *  @author  George Peck
- *  @version Oct 13, 2014
- *  @author  Period: 1-7
- *  @author  Assignment: A11_3Checker
- *
- *  @author  Sources: none
- */
-public interface Checker
-{
-    /**
-     * @param text  a string to consider for acceptance
-     * @return true if this Checker accepts text; false otherwise
-     */
-    boolean accept( String text );
-}
+public class Checker {
 
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+
+    }
+
+}
 
